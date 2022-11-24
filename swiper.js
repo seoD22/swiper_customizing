871b6560eaf80cd301b94e88d07b63f9cb5f1177
seoD22 <cobbt258@naver.com>
2022-11-24 12:51:05 +0900

    스와이퍼 커스터마이징_PC, mobile버전제작__1124

--- swiper.js
+++ swiper.js
@@ -1,14 +1,16 @@
-window.onload = function(){
+$(document).ready(function(){
+  var _width = $(window).width()
 
-  const loop_auto_center_3={
+  const loop_auto_center_3_pc={
     loop: true,
+    containerModifierClass: 'pc_slide-',
     slidesPerView: 3,
     centeredSlides: true,
 
     pagination: {
       el: '#banner.swiper .swiper-pagination',
+      clickable: true,
     },
-  
     navigation: {
       nextEl: '#banner.swiper .swiper-button-next',
       prevEl: '#banner.swiper .swiper-button-prev',
@@ -18,8 +20,35 @@ window.onload = function(){
       disableOnInteraction: false,
       pauseOnMouseEnter:	true
     }
-  
   }
 
-  const swiper = new Swiper('#banner.swiper', loop_auto_center_3);
-}
+  const loop_auto_center_3_m={
+    loop: true,
+    slidesPerView: 1.5,
+    pagination: {
+      el: '#banner.swiper .swiper-pagination',
+      type: "fraction",
+    },
+  
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    }
+  }
+
+  // 스와이퍼 세팅 2가지 설정(모바일, 데스크탑)
+  var swiper_mbanner = undefined; //타임설정 하지 않음 / 스와이퍼 실행을 저장할 함수
+  if(_width < 768){ 
+    if(swiper_mbanner != undefined){ // 이미 이전에 스와이퍼 사용 이력이 있다면
+      swiper_mbanner.destroy() // 파괴하고
+    }
+    swiper_mbanner = new Swiper('#banner.swiper', loop_auto_center_3_m);
+  }else{
+    if(swiper_mbanner != undefined){
+      swiper_mbanner.destroy()
+    }
+    swiper_mbanner = new Swiper('#banner.swiper', loop_auto_center_3_pc);
+  } 
+
+
+})
